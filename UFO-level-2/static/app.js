44dda9a9fad977be21a480f539ad0c42f16a3592
Kevin McConchie <Kevin.McConchie@outlook.com>
2022-08-14 15:16:06 +1000

    Update

--- UFO-level-2/static/app.js
+++ UFO-level-2/static/app.js
@@ -10,7 +10,6 @@ tableData.forEach(function(sighting){
     console.log(sighting);
 });
 
-
 // Use d3 to update cells text with sightings information
 tableData.forEach(function(sighting) {
     console.log(sighting);
@@ -25,7 +24,7 @@ tableData.forEach(function(sighting) {
 });
 
 // Select the button
-var button = d3.select("#button");
+var button = d3.select("#filter-btn");
 
 // // Select the form
 var form = d3.select("#form");
@@ -34,45 +33,53 @@ var form = d3.select("#form");
 button.on("click", runEnter);
 form.on("submit",runEnter);
 
-// // Complete the event handler function for the form
+// Complete the event handler function for the form
 function runEnter() {
 
-// // Get the value property of the input elements and set all text to lowercase
+// Get the value property of the input elements and set all text to lowercase
 var dateTime = d3.select("#datetime").property("value");
 
-// var country = d3.select("#country").property("value").toLowerCase();
+var country = d3.select("#country").property("value").toLowerCase();
 
-// var state = d3.select("#state").property("value").toLowerCase();
+var state = d3.select("#state").property("value").toLowerCase();
 
-// var city = d3.select("#city").property("value").toLowerCase();
+var city = d3.select("#city").property("value").toLowerCase();
 
-// var shape = d3.select("#shape").property("value").toLowerCase();
-
-
+var shape = d3.select("#shape").property("value").toLowerCase();
 
 // create search result variable for each field
 
-if (dateTime)
-var searchResult = tableData.filter(dateTime => dateTime.datetime === inputValue);
+// if (dateTime)
+var searchResult = tableData.filter(result => result.datetime === dateTime);
 
+// if (country)
+var searchResult = tableData.filter(result => result.country === country);
 
+// if (state)
+var searchResult = tableData.filter(result => result.state === state);
+
+// if (city)
+var searchResult = tableData.filter(result => result.city === city);
+
+// if (shape)
+var searchResult = tableData.filter(result => result.shape === shape);
+
+// Check results
 console.log(searchResult);
-
-
 
 // Clear the table for search results
 tbody.html("");
 
-// // for loop to repopulate table with search results
-// searchResult.forEach(function(result) {
-//   console.log(result);
-//   var row = tbody.append("tr");
-//   Object.entries(result).forEach(function([key, value]) { console.log('key')
-//   console.log(key, value);
-//     // Append a cell to the row for each value
-//     // in the sighting table object
-//   var cell = row.append("td");
-//   cell.text(value);
-//   });
-// });
+// for loop to repopulate table with search results
+searchResult.forEach(function(result) {
+  console.log(result);
+  var row = tbody.append("tr");
+  Object.entries(result).forEach(function([key, value]) { console.log('key')
+  console.log(key, value);
+    // Append a cell to the row for each value
+    // in the sighting table object
+  var cell = row.append("td");
+  cell.text(value);
+  });
+});
 }
